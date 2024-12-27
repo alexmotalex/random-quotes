b233191df2d4ca52694ed72048b692b89ace6253
Oleksandr <tokyopac@gmail.com>
2024-12-27 20:45:28 +0200

    Show favorite quotes below current quote

--- quotes.js
+++ quotes.js
@@ -2,45 +2,55 @@ const quotes = [
   {
     quote: 'The best way to predict the future is to create it',
     author: 'Peter Drucker',
+    isFavorite: false,
   },
   {
     quote:
       'Success is not final, failure is not fatal: It is the courage to continue that count',
     author: 'Winston Churchill',
+    isFavorite: false,
   },
   {
     quote: "Don't watch the clock; do what it does. Keep going",
     author: 'Sam Levenson',
+    isFavorite: false,
   },
   {
     quote:
       'Keep your face always toward the sunshine—and shadows will fall behind you',
     author: 'Walt Whitman',
+    isFavorite: false,
   },
   {
     quote: 'Believe you can and you’re halfway there',
     author: 'Theodore Roosevelt',
+    isFavorite: false,
   },
   {
     quote: 'Do what you can, with what you have, where you are',
     author: 'Theodore Roosevelt',
+    isFavorite: false,
   },
   {
     quote: 'In the middle of every difficulty lies opportunity',
     author: 'Albert Einstein',
+    isFavorite: false,
   },
   {
     quote:
       'What lies behind us and what lies before us are tiny matters compared to what lies within us',
     author: 'Ralph Waldo Emerson',
+    isFavorite: false,
   },
   {
     quote: 'Act as if what you do makes a difference. It does',
     author: 'William James',
+    isFavorite: false,
   },
   {
     quote: 'It always seems impossible until it’s done',
     author: 'Nelson Mandela',
+    isFavorite: false,
   },
 ];
 
