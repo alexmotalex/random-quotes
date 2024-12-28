2b5189f44e19976365d922f7559b493ca785b4e7
Alex <tokyopac@gmail.com>
2024-12-28 23:08:23 +0200

    Refactoring - create favorites and quote modules

---
+++ src/data/quotes.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    text: 'The best way to predict the future is to create it',
+    author: 'Peter Drucker',
+    isFavorite: false,
+  },
+  {
+    text: 'Success is not final, failure is not fatal: It is the courage to continue that count',
+    author: 'Winston Churchill',
+    isFavorite: false,
+  },
+  {
+    text: "Don't watch the clock; do what it does. Keep going",
+    author: 'Sam Levenson',
+    isFavorite: false,
+  },
+  {
+    text: 'Keep your face always toward the sunshine—and shadows will fall behind you',
+    author: 'Walt Whitman',
+    isFavorite: false,
+  },
+  {
+    text: 'Believe you can and you’re halfway there',
+    author: 'Theodore Roosevelt',
+    isFavorite: false,
+  },
+  {
+    text: 'Do what you can, with what you have, where you are',
+    author: 'Theodore Roosevelt',
+    isFavorite: false,
+  },
+  {
+    text: 'In the middle of every difficulty lies opportunity',
+    author: 'Albert Einstein',
+    isFavorite: false,
+  },
+  {
+    text: 'What lies behind us and what lies before us are tiny matters compared to what lies within us',
+    author: 'Ralph Waldo Emerson',
+    isFavorite: false,
+  },
+  {
+    text: 'Act as if what you do makes a difference. It does',
+    author: 'William James',
+    isFavorite: false,
+  },
+  {
+    text: 'It always seems impossible until it’s done',
+    author: 'Nelson Mandela',
+    isFavorite: false,
+  },
+];
+
+export default quotes;
